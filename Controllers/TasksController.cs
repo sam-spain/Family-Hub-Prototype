4dc24c7e7b7cc6ba33367609b9f3c16af80c525a
Sam Spain <sam@samspain.com>
2023-04-08 10:22:16 +0100

    Add base project with custom tasks page

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class TasksController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index(string name)
+    {
+        var view = "There will be a list of tasks here someday!";
+        if(name != "") {
+            view += "\nThese tasks will be shown for: " + name;
+        }
+        return view;
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace family_hub_prototype.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
