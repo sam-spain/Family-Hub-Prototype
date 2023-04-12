9618efa9e258fcfc83193c5540482aee959ddf26
Sam Spain <sam@samspain.com>
2023-04-12 07:35:50 +0100

    Make tasks page. Add link to page in header

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -5,20 +5,8 @@ namespace MvcMovie.Controllers;
 
 public class TasksController : Controller
 {
-    // 
-    // GET: /HelloWorld/
-    public string Index(string name)
+    public IActionResult Index()
     {
-        var view = "There will be a list of tasks here someday!";
-        if(!String.IsNullOrEmpty(name)) {
-            view += " These tasks will be shown for: " + name;
-        }
-        return HtmlEncoder.Default.Encode(view);
-    }
-    // 
-    // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
-    {
-        return "This is the Welcome action method...";
+        return View();
     }
 }
